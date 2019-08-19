c45ff215d23f28e3a5364f749e0ff562d2182e30
github username <github email address>
2019-08-19 13:08:05 -0400

    added api data

--- Gemfile
+++ Gemfile
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
 
-gem "sinatra-activerecord"
+
 gem "sqlite3"
 gem "pry"
 gem "require_all"
 gem "faker"
 gem "rest-client" 
+gem 'sinatra'
+gem 'thin'
+gem 'activerecord', '~>5.2.3'
+gem 'sinatra-activerecord'
+gem 'rake'
+
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
 require 'bundler'
+require 'rest-client'
+require 'json'
+
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
+
+url = 'https://data.cityofnewyork.us/resource/kpav-sd4t.json'
+response = RestClient.get(url)
+JSON.parse(response)
