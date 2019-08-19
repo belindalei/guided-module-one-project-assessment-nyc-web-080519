8de7ea2d8ce0c430de7a7e8941512ea25a0a2001
github username <github email address>
2019-08-19 17:50:17 -0400

    added likeJob method

--- lib/job_seeker.rb
+++ lib/job_seeker.rb
@@ -9,7 +9,7 @@ class JobSeeker < ActiveRecord::Base
         salary = gets.chomp
         puts "What is your government job level?"
         level = gets.chomp
-        JobSeeker.new(name: user_name, desired_salary: salary, level: level)
+        JobSeeker.create(name: user_name, desired_salary: salary, level: level)
     end
 
 #RETURN BACK TO MAKE SURE LIST OF MATCHES IS UNIQUE 
@@ -18,17 +18,24 @@ class JobSeeker < ActiveRecord::Base
         job_data = JSON.parse(RestClient.get("https://data.cityofnewyork.us/resource/kpav-sd4t.json?"))
         matches = job_data.select do |job|
             job["salary_range_from"].to_i >= self.desired_salary
-        end.
-        matches.each_with_index do |match, i|
+        end
+        matched_list = matches.each_with_index do |match, i|
            puts "#{i+1}. #{match['business_title']} located in #{match['work_location']} with job_id #{match['job_id']}"
         end
+        matched_list.save 
     end
 
 #User Story #3: As a job seeker, I want to be able to save jobs that I am interested in for later review
 
-    #be able to look through the list of job matches generated through salaryMatch 
-    #select jobs based on the index numbers listed 
-    #save to the job queue
+    def likeJob(job_id)
+        job_data = JSON.parse(RestClient.get("https://data.cityofnewyork.us/resource/kpav-sd4t.json?job_id=#{job_id}"))[0]
+        new_job = OpenJob.create(title: job_data["business_title"], description: job_data["job_description"], level: job_data["level"], salary_range_from: job_data["salary_range_from"], salary_range_to: job_data["salary_range_to"])
+        LikedJob.create("open_job_id" => new_job.id, "job_seeker_id" => self.id)
+    end
+
+#User Story #4: As a job seeker, I want to be able to remove jobs from the list that I am no longer intrested in.
+
+
 
 
 end
