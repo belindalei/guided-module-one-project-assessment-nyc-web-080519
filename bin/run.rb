bb686f54d24edbdfe39ea8c1bb652691e7b17d53
DanBehrman <dan.behrman@wework.com>
2019-08-19 20:26:19 -0400

    started cli and run files

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,7 @@
 require_relative '../config/environment'
 
-puts "hello world"
-
+#puts "hello world"
+CLI.new.run
 # url = 'https://data.cityofnewyork.us/resource/kpav-sd4t.json'
 # response = RestClient.get(url)
 # JSON.parse(response)
