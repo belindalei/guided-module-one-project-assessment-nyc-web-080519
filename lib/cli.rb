0a182a8779c923ff1b99f27491dd62e863771be8
DanBehrman <dan.behrman@wework.com>
2019-08-19 22:01:11 -0400

    added some formatting for cli menu

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,13 +3,13 @@ require 'pry'
 
 class CLI
     def run
-        puts "Welcome to Belinda and Dan's Job-Seeking App!"
+        puts "\nWelcome to Belinda and Dan's Job-Seeking App!"
         main_menu
     end
 
     def main_menu
-        puts "Please choose from an option from the list below"
-        puts "1. Create a new profile."
+        puts "\nPlease choose from an option from the list below:"
+        puts "\n1. Create a new profile."
         puts "2. Show me a list of job that match my profile. (Salary)"
         puts "3. Delete a job from my 'liked' list."
         puts "4. Exit."
@@ -19,12 +19,12 @@ class CLI
 
     def menu_router(input)
         if input.class != Integer || input < 1 || input > 4
-            puts "Sorry. That's not a valid menu selection. Try again!"
+            puts "\nSorry. That's not a valid menu selection. Try again!"
             main_menu
         elsif input == 1
             JobSeeker.createProfile
         else
-            puts "I don't support that option yet. Eek."
+            puts "\nI don't support that option yet. Eek."
             main_menu
         end
     end
