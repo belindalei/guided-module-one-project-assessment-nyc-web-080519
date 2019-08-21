900b2acb220d3b9d4c10a2f48f2df59799fd1b7c
github username <github email address>
2019-08-21 19:30:52 -0400

    Added helper methods / refactoring

--- lib/cli.rb
+++ lib/cli.rb
@@ -24,7 +24,7 @@ class CLI
         my_name = gets.chomp
         if JobSeeker.find_by name: my_name
           job_seeker = JobSeeker.find_by name: my_name
-            puts `clear`
+            clear
             puts "\nHello #{job_seeker.name}. Welcome back!"
         else
             job_seeker = JobSeeker.create_profile(my_name)
@@ -32,17 +32,10 @@ class CLI
         main_menu(job_seeker)
     end
 
+
     def main_menu(job_seeker)
-        puts "\nPlease choose from an option from the list below:"
-
-        puts "\n1. Show me a list of jobs that match my profile. (Sorted by salary)"
-        puts "2. Show me the list of jobs on my 'liked' list."
-        puts "3. Delete a job from my 'liked' list."
-        puts "4. Add a note to a job from my 'liked' list.\n"
-        puts "5. Exit.\n"
-        print "\nPlease enter your choice: "
+        print_menu_options
         input = gets.chomp.to_i
-
         menu_router(input, job_seeker)
     end
 
@@ -50,61 +43,44 @@ class CLI
         if input.class != Integer || input < 1 || input > 5
             puts "\nSorry. That's not a valid menu selection. Try again!"
         elsif input == 1
-            stars
-            job_seeker.get_matches
-            stars 
-            puts "\nPlease choose a Job ID you would like to add, or press '0' to return to the main menu.\n\n"
-            print "Your choice: "
-            job_id = gets.chomp.to_i 
-            if job_id == 0
-                puts `clear`
-                main_menu(job_seeker)
-            end
-            job_seeker.like_job(job_id)
-            var = job_seeker.open_jobs.where(api_job_id: job_id).first.title
-            puts "\n#{job_id} - #{var} added!"
-            print "\nWould you like to add another? (Y/N): "
-            answer = gets.chomp 
-            while answer == 'Y' || answer == 'y'
-                stars
-                print "\nPlease choose a Job ID you would like to add, or press '0' to return to the main menu.\n"
+            print_with_stars(job_seeker.get_matches)
+            while true 
+                puts "\nPlease choose a Job ID you would like to add, or press '0' to return to the main menu.\n\n"
+                print "Your choice: "
                 job_id = gets.chomp.to_i 
-                job_seeker.like_job(job_id) 
-                puts "\n#{job_id} - #{var} added!"
-                print "\nWould you like to add another? (Y/N): "
-                answer = gets.chomp
+                if job_id == 0
+                    clear 
+                    main_menu(job_seeker)
+                end
+                puts job_seeker.like_job(job_id)
+                job_seeker.display_liked_jobs
             end 
-            job_seeker.display_liked_jobs
-            puts "The above are your liked jobs."
-            elsif input == 2 
-            puts `clear`
-            stars
-            job_seeker.display_liked_jobs
-            stars
+        elsif input == 2 
+            clear
+            print_with_stars(job_seeker.display_liked_jobs)
         elsif input == 3
-            puts `clear`
-            stars 
-            puts "You have chosen to delete items from your list.\n\n"
-            job_seeker.display_liked_jobs
-            puts "\nWhich item would you like to delete from the table above?\n"
-            print "\nJob_ID number to delete: "
+            clear
+            print_with_stars(
+                "You have chosen to delete items from your list.\n\n" +
+                job_seeker.display_liked_jobs +
+                "\nWhich item would you like to delete from the table above?\n" +
+                "\nJob_ID number to delete: "
+            )
             delete_item = gets.chomp.to_i 
-            stars 
             job_seeker.remove_liked_job(delete_item)
             puts "You have successfully removed #{delete_item}."
         elsif input == 4 
-            stars 
-            puts "\nYou have chosen to add a note to a job from your 'liked' list.\n"
-            job_seeker.display_liked_jobs
+            puts "\nYou have chosen to add a note to a job from your 'liked' list.\n" 
+            job_seeker.display_liked_jobs 
             puts "\nPlease choose which job you would like to add a note to based on its JOB_ID number in the list. "
             job_note_id = gets.chomp.to_i 
-            puts `clear`
+            clear 
             puts "\nYou have selected #{job_note_id} to add a note to. \n\nPlease type in the note that you would like to add: \n\n"
             note = gets.chomp.to_s 
             job_seeker.add_notes(job_note_id, note)
             job_seeker.display_liked_jobs 
             puts "Your #{job_note_id} has been updated with the following note: \n #{note}"
-            stars
+            
         elsif input == 5 
             puts "\nThanks for logging in. Have a nice day!"
             exit 0 
@@ -115,7 +91,23 @@ class CLI
         main_menu(job_seeker)
     end
     
-    def stars
-        puts "\n************************************************************\n\n" 
+    def print_with_stars(arg)
+       puts "\n************************************************************\n\n" + arg + "\n************************************************************\n\n"
     end
+
+    def print_menu_options
+        puts "\nPlease choose from an option from the list below:\n\n" +
+        "1. Show me a list of jobs that match my profile. (Sorted by salary)\n" +
+        "2. Show me the list of jobs on my 'liked' list.\n" +
+        "3. Delete a job from my 'liked' list.\n" +
+        "4. Add a note to a job from my 'liked' list.\n" +
+        "5. Exit.\n" 
+        
+        print "\nPlease enter your choice: "
+    end 
+
+    def clear 
+        puts `clear`
+    end
+
 end
